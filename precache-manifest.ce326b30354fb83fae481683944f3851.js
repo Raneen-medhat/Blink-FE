f547f724703d8a1395bb029427d7d925e56024b4
Raneen <raneen.medhat@gmail.com>
2020-05-17 17:25:53 +0200

    Updates

---
+++ precache-manifest.ce326b30354fb83fae481683944f3851.js
@@ -0,0 +1,278 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93ca39cf791a424257910cabcdeb2fa6",
+    "url": "/Blink-FE/index.html"
+  },
+  {
+    "revision": "a78773aab9a4e1093bc7",
+    "url": "/Blink-FE/static/css/main.bf092195.chunk.css"
+  },
+  {
+    "revision": "74d9b70cc16ea301a953",
+    "url": "/Blink-FE/static/js/2.44a5fad2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Blink-FE/static/js/2.44a5fad2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a78773aab9a4e1093bc7",
+    "url": "/Blink-FE/static/js/main.d81d260b.chunk.js"
+  },
+  {
+    "revision": "5675c67cb4876de54ada",
+    "url": "/Blink-FE/static/js/runtime-main.7e2cdc21.js"
+  },
+  {
+    "revision": "14b3a355f612d6181e891efd2c798b5f",
+    "url": "/Blink-FE/static/media/Gotham-Black.14b3a355.svg"
+  },
+  {
+    "revision": "1e4f22b86e58510e678ced6d1b7d0be2",
+    "url": "/Blink-FE/static/media/Gotham-Black.1e4f22b8.eot"
+  },
+  {
+    "revision": "2b5485a82bb9b9fb0fcf3bf53e742272",
+    "url": "/Blink-FE/static/media/Gotham-Black.2b5485a8.woff"
+  },
+  {
+    "revision": "91e0bcb57f4de7873ebaed73893af49f",
+    "url": "/Blink-FE/static/media/Gotham-Black.91e0bcb5.ttf"
+  },
+  {
+    "revision": "2815d534c716a929b5aa10f4af22271a",
+    "url": "/Blink-FE/static/media/Gotham-Bold.2815d534.woff"
+  },
+  {
+    "revision": "9c35bf87f23c8cca614720126fe0baa0",
+    "url": "/Blink-FE/static/media/Gotham-Bold.9c35bf87.svg"
+  },
+  {
+    "revision": "a0bbb07abd112430334bf9b38cc486e8",
+    "url": "/Blink-FE/static/media/Gotham-Bold.a0bbb07a.ttf"
+  },
+  {
+    "revision": "d06d161ee7ae8e5fe2cd890e486e4156",
+    "url": "/Blink-FE/static/media/Gotham-Bold.d06d161e.eot"
+  },
+  {
+    "revision": "188851ed3a03356a4a8eb3d43f9b8f85",
+    "url": "/Blink-FE/static/media/Gotham-BookItalic.188851ed.eot"
+  },
+  {
+    "revision": "63555c544874f2b53e5705d164c98db7",
+    "url": "/Blink-FE/static/media/Gotham-BookItalic.63555c54.ttf"
+  },
+  {
+    "revision": "da84a99701996b4ba80f723c2412ab99",
+    "url": "/Blink-FE/static/media/Gotham-BookItalic.da84a997.svg"
+  },
+  {
+    "revision": "e17105e51c65d1ff8960a5af52671e6b",
+    "url": "/Blink-FE/static/media/Gotham-BookItalic.e17105e5.woff"
+  },
+  {
+    "revision": "3412bad26546bdd9d88dc7d5a63b0046",
+    "url": "/Blink-FE/static/media/Gotham-Light.3412bad2.ttf"
+  },
+  {
+    "revision": "3c59ed2be4591aa8c1e6767a952b8dc7",
+    "url": "/Blink-FE/static/media/Gotham-Light.3c59ed2b.eot"
+  },
+  {
+    "revision": "f3f36544d8c63b17dc7ed4b909922c05",
+    "url": "/Blink-FE/static/media/Gotham-Light.f3f36544.woff"
+  },
+  {
+    "revision": "f76e3adf545b3299f643fd7642800351",
+    "url": "/Blink-FE/static/media/Gotham-Light.f76e3adf.svg"
+  },
+  {
+    "revision": "09448abe10e35adf4c2eac229274e798",
+    "url": "/Blink-FE/static/media/Gotham-Thin.09448abe.eot"
+  },
+  {
+    "revision": "47d577090754754c62bb107c569c6a8b",
+    "url": "/Blink-FE/static/media/Gotham-Thin.47d57709.ttf"
+  },
+  {
+    "revision": "4ac7dbb4f9f9f467db596e759b8bf7db",
+    "url": "/Blink-FE/static/media/Gotham-Thin.4ac7dbb4.svg"
+  },
+  {
+    "revision": "b4d4ca933210124def6162ac5cee69e4",
+    "url": "/Blink-FE/static/media/Gotham-Thin.b4d4ca93.woff"
+  },
+  {
+    "revision": "07e1c219c09cb820242561b7bd44cab8",
+    "url": "/Blink-FE/static/media/Gotham-ThinItalic.07e1c219.ttf"
+  },
+  {
+    "revision": "1bcefa0f6561d3458a5e918efee78b92",
+    "url": "/Blink-FE/static/media/Gotham-ThinItalic.1bcefa0f.eot"
+  },
+  {
+    "revision": "c7d88301cb82859d635349081ea87638",
+    "url": "/Blink-FE/static/media/Gotham-ThinItalic.c7d88301.svg"
+  },
+  {
+    "revision": "e1d24ff88b2acfc7776051573bf39e10",
+    "url": "/Blink-FE/static/media/Gotham-ThinItalic.e1d24ff8.woff"
+  },
+  {
+    "revision": "51edb577f2afccf6d5684e808cd52d08",
+    "url": "/Blink-FE/static/media/Gotham-UltraItalic.51edb577.woff"
+  },
+  {
+    "revision": "bbd2235b489c69539f96cb4fd44d875f",
+    "url": "/Blink-FE/static/media/Gotham-UltraItalic.bbd2235b.eot"
+  },
+  {
+    "revision": "cbd7e9b2b7b173041a114ce35698b0df",
+    "url": "/Blink-FE/static/media/Gotham-UltraItalic.cbd7e9b2.svg"
+  },
+  {
+    "revision": "f83bb8cef576305e5428b80fb6364a59",
+    "url": "/Blink-FE/static/media/Gotham-UltraItalic.f83bb8ce.ttf"
+  },
+  {
+    "revision": "30a89cb94eaf1b746ba332c02d5cc18f",
+    "url": "/Blink-FE/static/media/Gotham-XLight.30a89cb9.woff"
+  },
+  {
+    "revision": "3f6969ada534de188f1956dd87359296",
+    "url": "/Blink-FE/static/media/Gotham-XLight.3f6969ad.svg"
+  },
+  {
+    "revision": "93276171e8983788d6885a80681c8cbb",
+    "url": "/Blink-FE/static/media/Gotham-XLight.93276171.ttf"
+  },
+  {
+    "revision": "b04edd423dc68b3241ef7fbebc411442",
+    "url": "/Blink-FE/static/media/Gotham-XLight.b04edd42.eot"
+  },
+  {
+    "revision": "0830b44514e791c7cc43f0a194b38564",
+    "url": "/Blink-FE/static/media/Gotham-XLightItalic.0830b445.svg"
+  },
+  {
+    "revision": "1b1f0639dde12bb2dad2ececf9061440",
+    "url": "/Blink-FE/static/media/Gotham-XLightItalic.1b1f0639.ttf"
+  },
+  {
+    "revision": "ce2538d251b73f229982087e25f5c4e1",
+    "url": "/Blink-FE/static/media/Gotham-XLightItalic.ce2538d2.woff"
+  },
+  {
+    "revision": "e3a360d8f00cb7f8dde900a731d5825b",
+    "url": "/Blink-FE/static/media/Gotham-XLightItalic.e3a360d8.eot"
+  },
+  {
+    "revision": "8ce8e6698922e6ee0189fc34a9fca07b",
+    "url": "/Blink-FE/static/media/GothamBoldItalic.8ce8e669.woff"
+  },
+  {
+    "revision": "9b92f7fc1500a19b09dc9053fbe46b0c",
+    "url": "/Blink-FE/static/media/GothamBoldItalic.9b92f7fc.ttf"
+  },
+  {
+    "revision": "cc18832fe117844fe956b7ff04df0ae0",
+    "url": "/Blink-FE/static/media/GothamBoldItalic.cc18832f.svg"
+  },
+  {
+    "revision": "d120dbbacbcc424081a60964da8577ac",
+    "url": "/Blink-FE/static/media/GothamBoldItalic.d120dbba.eot"
+  },
+  {
+    "revision": "66ffcf47ea5cf16280ac2184ac751dd2",
+    "url": "/Blink-FE/static/media/GothamBook.66ffcf47.woff"
+  },
+  {
+    "revision": "96226a92b2d2c0739266785f997e29bf",
+    "url": "/Blink-FE/static/media/GothamBook.96226a92.eot"
+  },
+  {
+    "revision": "a53cbb6d48850bd168ac68b94d43ff91",
+    "url": "/Blink-FE/static/media/GothamBook.a53cbb6d.svg"
+  },
+  {
+    "revision": "b54724f54d4dd3f6796e3c4cc422f998",
+    "url": "/Blink-FE/static/media/GothamBook.b54724f5.ttf"
+  },
+  {
+    "revision": "0f0e43f48efb679501feff0240c3eb55",
+    "url": "/Blink-FE/static/media/GothamLightItalic.0f0e43f4.ttf"
+  },
+  {
+    "revision": "54d5c7643b0f73120a5774b03ddcebae",
+    "url": "/Blink-FE/static/media/GothamLightItalic.54d5c764.woff"
+  },
+  {
+    "revision": "67ede7688174b01ef7b4ae903d11075e",
+    "url": "/Blink-FE/static/media/GothamLightItalic.67ede768.svg"
+  },
+  {
+    "revision": "e87b4d95fa11a302b1884acfe34c22ee",
+    "url": "/Blink-FE/static/media/GothamLightItalic.e87b4d95.eot"
+  },
+  {
+    "revision": "31e297f45f4c6b08b73240fee380fd4e",
+    "url": "/Blink-FE/static/media/GothamMedium.31e297f4.eot"
+  },
+  {
+    "revision": "4dee94fccf01f50027bfa8ea80927e41",
+    "url": "/Blink-FE/static/media/GothamMedium.4dee94fc.svg"
+  },
+  {
+    "revision": "77171d8f5b5283f9d47a3434704bf944",
+    "url": "/Blink-FE/static/media/GothamMedium.77171d8f.ttf"
+  },
+  {
+    "revision": "b98652ef82bc0b3900258eed3b9f804f",
+    "url": "/Blink-FE/static/media/GothamMedium.b98652ef.woff"
+  },
+  {
+    "revision": "3cb38adcd96c75a3abb43c03b59f7d24",
+    "url": "/Blink-FE/static/media/GothamMediumItalic.3cb38adc.eot"
+  },
+  {
+    "revision": "721d8fd8eeea2f3d82d7cad3a87a5f5c",
+    "url": "/Blink-FE/static/media/GothamMediumItalic.721d8fd8.woff"
+  },
+  {
+    "revision": "b17502ab96cf3717a3b64307cf03b0ce",
+    "url": "/Blink-FE/static/media/GothamMediumItalic.b17502ab.svg"
+  },
+  {
+    "revision": "b1951be38b528a44704a217b9c44ab55",
+    "url": "/Blink-FE/static/media/GothamMediumItalic.b1951be3.ttf"
+  },
+  {
+    "revision": "29def3e5dd923346cc9efa53b9bc0b65",
+    "url": "/Blink-FE/static/media/contacts.29def3e5.svg"
+  },
+  {
+    "revision": "1f301d23fe1719f50ac01ebb5d85cf6e",
+    "url": "/Blink-FE/static/media/conversation.1f301d23.svg"
+  },
+  {
+    "revision": "fa2c07daf87d1ec998638f37e1412080",
+    "url": "/Blink-FE/static/media/home.fa2c07da.svg"
+  },
+  {
+    "revision": "144ff03084284c8876293946a229fd8e",
+    "url": "/Blink-FE/static/media/logout.144ff030.svg"
+  },
+  {
+    "revision": "2c93ec1d913d39b6fd787df1b200fb52",
+    "url": "/Blink-FE/static/media/search.2c93ec1d.svg"
+  },
+  {
+    "revision": "56611ebbb537dc7a98e2de3cee734acc",
+    "url": "/Blink-FE/static/media/send.56611ebb.svg"
+  },
+  {
+    "revision": "9f07a3df80b5eea9cfdcf9616e3d03d5",
+    "url": "/Blink-FE/static/media/setting.9f07a3df.svg"
+  }
+]);
